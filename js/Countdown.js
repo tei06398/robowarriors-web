8d7716db5fba3750b5f4ca2fa163ec3663a00576
aminameow4 <aminakitty2002@gmail.com>
2019-01-20 14:19:18 -0500

    updated countdown

--- js/Countdown.js
+++ js/Countdown.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 // Date set
-var countDownDate = new Date("Jan 19, 2019 07:30:00").getTime();
+var countDownDate = new Date("Feb 10, 2019 07:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
